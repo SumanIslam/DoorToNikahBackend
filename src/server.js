c8a5674b4fd8a0090ed510928c2aa7ea7054aeb1
Suman Islam <sumanislam1416@gmail.com>
2022-10-17 12:15:30 +0600

    setup mongodb connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const { mongoConnect } = require('../services/mongodb-setup')
 
 require('dotenv').config();
 
@@ -7,6 +8,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-})
+async function startServer() {
+  mongoConnect();
+
+  server.listen(PORT, () => {
+		console.log(`server is running on port ${PORT}`);
+	});
+}
+
+startServer();
