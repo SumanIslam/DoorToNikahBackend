3b0e803fdbfa4adb10477087e0fdc94420505e8f
Suman Islam <sumanislam1416@gmail.com>
2022-12-21 20:33:37 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', req.headers.origin);
 	res.header(
 		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept'
+		'Origin, X-Requested-With, Content-Type, Accept',
+		'Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE';
 	);
 	next();
 });
