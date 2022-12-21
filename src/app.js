1b56a1b4773cb0af0ac154facb9efce09f353fc1
Suman Islam <sumanislam1416@gmail.com>
2022-12-21 20:40:08 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -42,8 +42,9 @@ app.use(function (req, res, next) {
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept',
-		'Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE';
 	);
+	res.header('Access-Control-Allow-Credentials', true);
+  	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
 	next();
 });
 
